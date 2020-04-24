392999c6cb19ec4b3b944ebc2a6a4f3e2012653b
codimiracle <codimiracle@outlook.com>
2020-04-24 12:06:15 +0800

    add comments

--- src/main/java/com/codimiracle/web/request/identifier/annotation/NonRepeatable.java
+++ src/main/java/com/codimiracle/web/request/identifier/annotation/NonRepeatable.java
@@ -1,15 +1,52 @@
 package com.codimiracle.web.request.identifier.annotation;
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 codimiracle
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Mark which request method should be check.
+ */
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface NonRepeatable {
+    /**
+     * only once, and don't repeat forever
+     */
     long NO_INTERVAL = -1;
+    /**
+     * just one seconds
+     */
     long DEFAULT_INTERVAL = 0;
 
+    /**
+     * how long will can be repeated.
+     *
+     * @return interval millisecond
+     */
     long interval() default NO_INTERVAL;
 }
--- src/main/java/com/codimiracle/web/request/identifier/provider/NonRepeatableJavaProviderImpl.java
+++ src/main/java/com/codimiracle/web/request/identifier/provider/NonRepeatableJavaProviderImpl.java
@@ -1,5 +1,27 @@
 package com.codimiracle.web.request.identifier.provider;
-
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 codimiracle
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -7,6 +29,10 @@ import org.springframework.stereotype.Component;
 
 import java.util.Objects;
 
+/**
+ * Using guava implements request unique checking.
+ * note: this is not long term checking.
+ */
 @Component
 @ConditionalOnMissingBean(NonRepeatableRedisProviderImpl.class)
 public class NonRepeatableJavaProviderImpl implements NonRepeatableProvider {
